3b9e87e917d5cc50729aa3a31228e04d5a5e5bd7
Dr Nic Williams <drnicwilliams@gmail.com>
2017-05-23 15:20:00 +1000

    catch PG::ConnectionBad and show static page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::NoDatabaseError, with: :no_database_error
+  rescue_from PG::ConnectionBad, with: :no_database_error
   protect_from_forgery with: :exception
+
+  def no_database_error
+    render "home/no_database_error"
+  end
 end
